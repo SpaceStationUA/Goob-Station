727a37de33ea1dc7ddf107ad41e2897611a05275
Vareniko <Rapt0r.Varenik@gmail.com>
2025-10-04 15:12:51 +0200

    Test

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -236,7 +236,7 @@ namespace Content.IntegrationTests.Tests
             "Chloris", // Goobstation
             "Serpentcrest", // Goobstation
             "BoxTP", // Pirate
-            "GlacierTP", // Pirate
+            //"GlacierTP", // Pirate
         };
 
         private static readonly ProtoId<EntityCategoryPrototype> DoNotMapCategory = "DoNotMap";
