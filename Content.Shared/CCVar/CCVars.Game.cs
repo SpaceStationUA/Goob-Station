191df3085e00496f09f92b8325d07eece60d18bf
SyrniyCoder <SkorostetskyKiril@gmail.com>
2026-04-09 22:54:40 +0300

    fix and new parallax

--- Content.Shared/CCVar/CCVars.Game.cs
+++ Content.Shared/CCVar/CCVars.Game.cs
@@ -97,7 +97,7 @@ public sealed partial class CCVars
     ///     Prototype to use for map pool.
     /// </summary>
     public static readonly CVarDef<string>
-        GameMapPool = CVarDef.Create("game.map_pool", "DefaultMapPoolTP", CVar.SERVERONLY); # Pirate Changes
+        GameMapPool = CVarDef.Create("game.map_pool", "DefaultMapPoolTP", CVar.SERVERONLY); // Pirate Changes
 
     /// <summary>
     ///     The depth of the queue used to calculate which map is next in rotation.
