6684312169f1c5d81f93652d5391d572aa86338c
v0id <>
2026-07-15 12:24:24 +0300

    Port Trauma Station nuclear reactor

---
+++ Content.Pirate.Shared/Nuclear/Centrifuge/NuclearCentrifugeVisuals.cs
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace Content.Pirate.Shared.Nuclear.Centrifuge;
+
+[Serializable, NetSerializable]
+public enum NuclearCentrifugeVisuals : byte
+{
+    Processing,
+    Layer
+}
