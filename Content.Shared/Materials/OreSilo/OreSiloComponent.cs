265cc6eeaef6a03f11692729826fc6359e8c0ce6
CyberCore <149888090+VCStudio2@users.noreply.github.com>
2026-07-15 13:10:53 +0300

    Фікс силоса
    Виправлено радіус дії силосу з 40 (якого не хватало на жодну з станцій) до 10000 якого хватить для чого завгодно (ймовірно все ще треба буде підключати машини в UI силоса)

--- Content.Shared/Materials/OreSilo/OreSiloComponent.cs
+++ Content.Shared/Materials/OreSilo/OreSiloComponent.cs
@@ -23,7 +23,7 @@ public sealed partial class OreSiloComponent : Component
     /// Default value should be big enough to span a single large department.
     /// </remarks>
     [DataField, AutoNetworkedField]
-    public float Range = 40f; // Goob - 20->40
+    public float Range = 10000f; // Pirate - 40->10000
 }
 
 [Serializable, NetSerializable]
