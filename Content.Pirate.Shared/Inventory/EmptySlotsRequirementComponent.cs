9c483924969ebadab840fd0368971640e43afb45
v0id <>
2026-07-18 01:46:52 +0300

    Fix MODsuit inventory imports

--- Content.Pirate.Shared/Inventory/EmptySlotsRequirementComponent.cs
+++ Content.Pirate.Shared/Inventory/EmptySlotsRequirementComponent.cs
@@ -1,3 +1,4 @@
+using Content.Shared.Inventory;
 using Content.Shared.Whitelist;
 using Robust.Shared.GameStates;
 
--- Content.Pirate.Shared/Inventory/EmptySlotsRequirementSystem.cs
+++ Content.Pirate.Shared/Inventory/EmptySlotsRequirementSystem.cs
@@ -1,3 +1,4 @@
+using Content.Shared.Inventory;
 using Content.Shared.Inventory.Events;
 using Content.Pirate.Shared.ModularSuit;
 using Content.Shared.Whitelist;
