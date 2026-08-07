40492f8200cf965d692e458c0f3ff51d8406aa06
CyberLanos <cyber.lanos00@gmail.com>
2026-08-07 11:32:47 +0200

    feat: feroxi water adaptation

---
+++ Content.Pirate.Shared/Wetness/Components/WaterAdaptedComponent.cs
@@ -0,0 +1,6 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Pirate.Shared.Wetness.Components;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class WaterAdaptedComponent : Component;
