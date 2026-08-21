272c6eafd6fd1ea3a9511160564974c70d7d1b31
Vareniko <Rapt0r.Varenik@gmail.com>
2026-08-21 08:14:45 +0200

    уєєє

--- Content.Shared/_Pirate/Humanoid/Prototypes/SpeciesPrototype.ClothingFallback.cs
+++ Content.Shared/_Pirate/Humanoid/Prototypes/SpeciesPrototype.ClothingFallback.cs
@@ -4,5 +4,6 @@ namespace Content.Shared.Humanoid.Prototypes;
 
 public sealed partial class SpeciesPrototype
 {
+    [DataField("clothingSpeciesFallback")]
     public Dictionary<string, ProtoId<SpeciesPrototype>> ClothingSpeciesFallback { get; private set; } = new();
 }
